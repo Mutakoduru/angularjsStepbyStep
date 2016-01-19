da645c8a7a8c2e4e297a842dd9e4d3b92eb751e5
ranadeepak <deepakrana11@gmail.com>
2016-01-19 19:06:40 +0530

    Revert "base project changes"
    This reverts commit 66f94083aabbc5e7c55e0d5004374e6816a974b4.

--- example-1(Hello World)/app.js
+++
@@ -1,8 +0,0 @@
-var helloApp = angular.module('helloApp', []);
-
-helloApp.controller('HelloController',
-	['$scope', function($scope) {
-		$scope.welcomeMsg = 'Hello World From AngularJS';
-
-	}
-]);
