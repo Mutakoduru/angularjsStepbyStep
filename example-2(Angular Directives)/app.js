acf19d857361bc95bf697d241029bd9ffc99e273
ranadeepak <deepakrana11@gmail.com>
2016-01-19 19:09:57 +0530

    base commit

---
+++ example-2(Angular Directives)/app.js
@@ -0,0 +1,10 @@
+var votingApp = angular.module('votingApp', []);
+
+votingApp.controller('VotingCntrl',
+	['$scope', function($scope) {
+		$scope.welcomeMsg = 'Test Your Voting Eligibility';
+		$scope.rules = ['You should be above 18 years of age',
+		'You should have a valid Voter Card',
+		'Your name should be listed in the area voter list'];
+	}
+]);
