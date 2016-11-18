c5710bc1adc6ea389d15381602b949a765ab3d2d
ranadeepak <deepakrana11@gmail.com>
2016-11-18 05:49:14 +0530

    minor modifications

--- example-3(Directive)/app.js
+++ example-3(Directive)/app.js
@@ -1,6 +1,6 @@
 /**
 This example calculates employee's maximum eligible amount under EAP.
-In the corresponding 'emp-emp-form.html' file we have declared Emp Name and CTC as required field 
+In the corresponding 'emp-eap-form.html' file we have declared Emp Name and CTC as required field 
 but there is no html element with '*' symbol in this file.
 We are creating this element in 'requiredDirective.js' via a custom directive.
 Further, we are using Angular Form Validation to know more about this refer: https://docs.angularjs.org/guide/forms 
