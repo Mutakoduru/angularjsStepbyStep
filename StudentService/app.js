72b87275e1a6d3035e5b4331435cc153cf2c70ad
Deepak Rana <deepakrana11@gmail.com>
2016-12-05 22:29:18 +0530

    filter example

--- StudentService/app.js
+++ StudentService/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('studentApp');
 
 app.controller('studentController', ['$scope', 'studentService', '$rootScope', function($scope, studentService, $rootScope) {
-    $scope.user = {name: 'Deepak', sub1: 29, sub2: 30, sub3: 24};
+    $scope.user = {name: 'Deepak', sub1: 29, sub2: 30, sub3: 24, fees: 1000000, pass: true};
 
     $scope.calculateTotal = function() {
         $scope.totalMarks = studentService.calculateTotal($scope.user.sub1, $scope.user.sub2, $scope.user.sub3);
